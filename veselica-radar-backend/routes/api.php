a7e6df28060104ccfbf790cf07b0f4f45bef82ad
JanSterle1337 <jan.sterle123@gmail.com>
2024-05-13 18:17:37 +0200

    Added simple login route(authentication) to my REST api

--- veselica-radar-backend/routes/api.php
+++ veselica-radar-backend/routes/api.php
@@ -8,5 +8,24 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::post('/sanctum/token', function (Request $request) {
+   $request->validate([
+       'email' => 'required|string|email',
+       'password' => 'required|string',
+       'device_name' => 'required|string'
+   ]);
+
+   $user = \App\Models\User::where('email', $request->input('email'))->first();
+
+   if (! $user || ! Hash::check($request->input('password'), $user->password)) {
+       throw \Illuminate\Validation\ValidationException::withMessages([
+           'email' => ['The provided credentials are incorrect.'],
+       ]);
+   }
+
+   return $user->createToken($request->input('device_name'))->plainTextToken;
+});
+
+
 Route::get('/events', [EventsController::class, 'index']);
 Route::get('/events/{id}', [EventsController::class, 'getById']);
